03d5d5e4a6997822e0632d8151738a5c2ade8cc6
andrew-wrisinger <36801813+andrew-wrisinger@users.noreply.github.com>
2018-04-09 00:01:25 -0400

    Update radio.py

--- radio.py
+++ radio.py
@@ -1,14 +1,15 @@
 # Import libraries.
 import requests
+import urllib2
 import re
 from bs4 import BeautifulSoup
 
-# Identify the URL and response headers.
-headers = {'Date': 'Mon, 02 Apr 2018'}
-url = requests.get('http://xpn.org/playlists/xpn-playlist', headers = headers)
-html = (url.text)
-soup = BeautifulSoup(html, 'html.parser')
+# Returns songs for a given date.
+def get_text(date):
+    url = requests.get('http://xpn.org/playlists/xpn-playlist', headers = {'Date': date})
+    soup = BeautifulSoup(url.text, 'html.parser')
+    songtext = soup.find(id= 'accordion').prettify()
+    print songtext
 
-# Identifies a href URLs.
-for link in soup.find_all('a'):
-    print(link.get('href'))
+# Testing
+get_text('Mon, 02 Apr 2018')
