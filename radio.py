ea0c7772bcb41fda04a9c873f80db4d4522fd101
andrew-wrisinger <36801813+andrew-wrisinger@users.noreply.github.com>
2018-12-06 02:58:24 -0800

    Update radio.py

--- radio.py
+++ radio.py
@@ -1,15 +1,15 @@
-# Import libraries.
 import requests
-import urllib2
-import re
 from bs4 import BeautifulSoup
 
 # Returns songs for a given date.
-def get_text(date):
-    url = requests.get('http://xpn.org/playlists/xpn-playlist', headers = {'Date': date})
+def get_text(playlistdate):
+    url = requests.post('http://xpn.org/playlists/xpn-playlist', data = {'playlistdate': playlistdate})
     soup = BeautifulSoup(url.text, 'html.parser')
-    songtext = soup.find(id= 'accordion').prettify()
-    print songtext
+    songtext = str(soup.find(id= 'accordion').find_all('a', href=True))
+    songtext.split(">,")
+    print(songtext)
 
-# Testing
-get_text('Mon, 02 Apr 2018')
+get_text('12-03-2018')
+
+if __name__ == '__main__'():
+    print'z'
